43aaa8b3acff42c995442f5c7f6e58dad852f486
Michał Radmacher <michal@radmacher.pl>
2013-12-09 22:25:30 +0100

    fix some display issues

--- public/js/karoryfer.js
+++ public/js/karoryfer.js
@@ -1,20 +1,23 @@
-$(window).load(function() {
-  var height = 0;
-  var img_height = 0;
-  $(".thumbnail").each(function() {
-    current_height = $(this).height();
-    if( current_height > height ) {
-      height = current_height;
-    }
-    current_img_height = $(this).find('img').height();
-    if( current_img_height > img_height ) {
-      img_height = current_img_height;
-    }
-  });
-  $(".thumbnail").each(function() {
-    $(this).height(height);
-    img = $(this).find('img');
-    img.css('margin-bottom', img_height - img.height());
+$(function() {
+  $(".thumbnails").each(function() {
+    var height = 0;
+    var img_height = 0;
+
+    $(this).find(".thumbnail").each(function() {
+      current_height = $(this).height();
+      if( current_height > height ) {
+        height = current_height;
+      }
+      current_img_height = $(this).find('img').height();
+      if( current_img_height > img_height ) {
+        img_height = current_img_height;
+      }
+    });
+    $(this).find(".thumbnail").each(function() {
+      $(this).height(height);
+      img = $(this).find('img');
+      img.css('margin-bottom', img_height - img.height());
+    });
   });
 });
 
