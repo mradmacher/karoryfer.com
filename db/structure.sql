2d5478cda1dcc2ba708f902730533453845114d7
Michał Radmacher <michal@radmacher.pl>
2013-12-30 22:08:22 +0100

    remove paperclip image from albums

--- db/structure.sql
+++ db/structure.sql
@@ -39,10 +39,6 @@ CREATE TABLE albums (
     artist_id integer NOT NULL,
     created_at timestamp without time zone DEFAULT now() NOT NULL,
     updated_at timestamp without time zone DEFAULT now() NOT NULL,
-    image_file_name character varying(40),
-    image_content_type character varying(32),
-    image_file_size integer,
-    image_updated_at timestamp without time zone,
     license_id integer,
     reference character varying(80) NOT NULL,
     donation text,
@@ -1066,3 +1062,5 @@ INSERT INTO schema_migrations (version) VALUES ('20131221223326');
 INSERT INTO schema_migrations (version) VALUES ('20131222223845');
 
 INSERT INTO schema_migrations (version) VALUES ('20131230200147');
+
+INSERT INTO schema_migrations (version) VALUES ('20131230210259');
