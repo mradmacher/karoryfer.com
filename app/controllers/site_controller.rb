76c79e0923aaab4e378301044492e36ba5c14675
Michał Radmacher <michal@radmacher.pl>
2014-02-08 22:48:40 +0100

    show all artists and albums on start page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,7 +2,7 @@ class SiteController < ApplicationController
   def home
     @current_posts = Post.some.published
     @current_events = Event.some.current.published
-    @videos = Video.some
-    @albums = Album.some.published
+    @albums = Album.published
+    @artists = Artist.order( :name )
   end
 end
