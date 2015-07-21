afa3d2e340998ee6b9ba38f724dfcd330fb0e226
Michał Radmacher <michal@radmacher.pl>
2015-07-21 23:22:21 +0200

    move some settings from initializer to settings model

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -2,20 +2,42 @@ class Settings
   class << self
     attr_accessor :filer_root
 
-    def values
-      @values ||= {}
-    end
-
-    def set(key, value)
-      values[key] = value
-    end
-
-    def get(key)
-      values[key]
-    end
-
     def filer
       Uploader::Filer.new(File.join(Rails.root, 'db', 'ftp'))
+    end
+
+    def donations_info_url
+      '/karoryfer-lecolds/informacje/darowizny'
+    end
+
+    def youtube_url
+      'http://www.youtube.com/user/KaroryferLecolds'
+    end
+
+    def facebook_url
+      'http://www.facebook.com/karoryfer'
+    end
+
+    def twitter_url
+      'http://www.twitter.com/karoryfer'
+    end
+
+    def flattr_url
+      'http://flattr.com/profile/karoryfer'
+    end
+
+    def gplus_url
+      'https://plus.google.com/101590945458103175168'
+    end
+
+    def highlighted
+      [
+        { 'title' => 'Sample',
+          'reference' => 'karoryfer-samples'},
+        {
+          'title' => 'O nas',
+          'reference' => 'karoryfer-lecolds'}
+      ]
     end
   end
 end
--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,17 +1,3 @@
-Settings.set 'donations_info_url', '/karoryfer-lecolds/informacje/darowizny'
-Settings.set 'karoryfer.youtube.url', 'http://www.youtube.com/user/KaroryferLecolds'
-Settings.set 'karoryfer.facebook.url', 'http://www.facebook.com/karoryfer'
-Settings.set 'karoryfer.twitter.url', 'http://www.twitter.com/karoryfer'
-Settings.set 'karoryfer.flattr.url', 'http://flattr.com/profile/karoryfer'
-Settings.set 'karoryfer.gplus.url', 'https://plus.google.com/101590945458103175168'
-
-Settings.set 'highlighted', [
-  { 'title' => 'Sample',
-    'reference' => 'karoryfer-samples'},
-  {
-    'title' => 'O nas',
-    'reference' => 'karoryfer-lecolds'}
-]
 Publisher.instance.name = 'Karoryfer Lecolds'
 Publisher.instance.url = 'http://www.karoryfer.com'
 Uploader::Release.album_store_dir = File.join( Rails.root, 'public', 'downloads', 'wydawnictwa' )
@@ -21,4 +7,3 @@ Track::Uploader.store_dir = File.join( Rails.root, 'db', 'tracks' )
 Uploader::ArtistImage.store_dir = File.join( Rails.root, 'public', 'uploads', 'obrazki', 'artysci' )
 Uploader::AlbumImage.store_dir = File.join( Rails.root, 'public', 'uploads', 'obrazki', 'wydawnictwa' )
 Uploader::EventImage.store_dir = File.join( Rails.root, 'public', 'uploads', 'obrazki', 'wydarzenia' )
-
