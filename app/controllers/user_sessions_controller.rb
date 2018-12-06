755c6e3b4cabf6c98b5aa3f6846a4d3a78150db5
Michał Radmacher <michal@radmacher.pl>
2018-12-07 00:30:53 +0100

    fix authlogic params

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(params[:user_session])
+    @user_session = UserSession.new(user_session_params.to_h)
     if @user_session.save
       redirect_to root_url
     else
@@ -20,5 +20,9 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     redirect_to root_url
+  end
+
+  def user_session_params
+    params.require(:user_session).permit(:login, :password)
   end
 end
