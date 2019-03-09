9b000a35a0a12a36e2dae8188fe14497598f771d
Michał Radmacher <michal@radmacher.pl>
2019-03-09 22:42:15 +0100

    migrate authlogic crypto provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   end
 
   acts_as_authentic do |c|
-    c.crypto_provider = Authlogic::CryptoProviders::Sha512
+    c.transition_from_crypto_providers = [Authlogic::CryptoProviders::Sha512]
+    c.crypto_provider = Authlogic::CryptoProviders::SCrypt
   end
 
   validates :admin, inclusion: { in: [true, false] }
